07bef04cec9e274210485e6c8d33f0b68ab80b42
Georgiy <georgii_sidenko@mail.ru>
2023-02-01 21:27:58 +0300

    task15

--- lesson2/Program.cs
+++ lesson2/Program.cs
@@ -4,6 +4,7 @@
 
 
 // Task 13
+// Console.Clear();
 // int number = 123;
 // int len = 1;
 // int temp = number;
@@ -22,3 +23,54 @@
 // int zeros = Convert.ToInt32(Math.Pow(10, len - 3));
 // Console.WriteLine((number / zeros) % 10);
 // }
+
+
+// Task 15
+// while (true)
+// {
+//     Console.Clear();
+//     Console.WriteLine("Введите число от 1 до 7");
+//     int n = Convert.ToInt32(Console.ReadLine());
+//     if(n > 0 && n < 8)
+//     {
+//         switch (n)
+//         {
+//             case 1:
+//                 Console.WriteLine("Понедельник");
+//                 break;
+
+//             case 2:
+//                 Console.WriteLine("Вторник");
+//                 break;
+
+//             case 3:
+//                 Console.WriteLine("Среда");
+//                 break;
+
+//             case 4:
+//                 Console.WriteLine("Четверг");
+//                 break;
+
+//             case 5:
+//                 Console.WriteLine("Пятница");
+//                 break;
+
+//             case 6:
+//                 Console.WriteLine("Суббота");
+//                 break;
+
+//             case 7:
+//                 Console.WriteLine("Воскресенье");
+//                 break;    
+//         }
+//         break;
+//     }
+//     else
+//     {   
+//         Console.Clear();
+//         Console.WriteLine("Вы ввели неверное число");
+//         Console.WriteLine("Введите число от 1 до 7");
+//         n = Convert.ToInt32(Console.ReadLine());
+//     }
+// }
+
