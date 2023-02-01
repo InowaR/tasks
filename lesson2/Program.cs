298adad37c7b884f5210559e07925512732bd7fa
Georgiy <georgii_sidenko@mail.ru>
2023-02-01 20:51:14 +0300

    init commit

---
+++ lesson2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Task 10
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine((number / 10) % 10);
+
+
+// Task 13
+// int number = 123;
+// int len = 1;
+// int temp = number;
+// while (len < temp)
+// {
+//     temp = temp / 10;
+//     len++;
+// }
+
+// if(len < 3)
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+// else
+// {
+// int zeros = Convert.ToInt32(Math.Pow(10, len - 3));
+// Console.WriteLine((number / zeros) % 10);
+// }
