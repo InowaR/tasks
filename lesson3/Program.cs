f1a20f794208b1da8f0d9594b4fdd0469f35832b
Georgiy <georgii_sidenko@mail.ru>
2023-02-05 22:47:59 +0300

    first task

---
+++ lesson3/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("start");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int length()
+{
+    int len = 1;
+    int temp = number;
+    while (len < temp)
+    {
+        temp = temp / 10;
+        len++;
+    }
+    return len;
+}
+
+int pow(int x, int i)
+{
+    int res = 1;
+    int count = 0;
+    while(count < i)
+    {
+        res *= x;
+        count++;
+    }
+    return res;
+}
+int len = length();
+int n = pow(10,(len/2) + 1);
+int left = number / n;
+int right = number % 100;
+Console.WriteLine($"{left}, {right}");
+Console.WriteLine($"{left == right}");
