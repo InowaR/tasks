62f8cefe3a346ea51969a2697977082fb2e06b05
Georgiy <georgii_sidenko@mail.ru>
2023-02-06 03:22:02 +0300

    task 19 palindrome

--- lesson3/Program.cs
+++ lesson3/Program.cs
@@ -1,7 +1,7 @@
-﻿Console.WriteLine("start");
+﻿Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int length()
+int length(int number)
 {
     int len = 1;
     int temp = number;
@@ -13,20 +13,59 @@ int length()
     return len;
 }
 
-int pow(int x, int i)
+int degree(int num, int degree)
 {
     int res = 1;
     int count = 0;
-    while(count < i)
+    while(count < degree)
     {
-        res *= x;
+        res *= num;
         count++;
     }
     return res;
 }
-int len = length();
-int n = pow(10,(len/2) + 1);
-int left = number / n;
-int right = number % 100;
-Console.WriteLine($"{left}, {right}");
-Console.WriteLine($"{left == right}");
+
+int reverse(int number)
+{
+    int len = length(number);
+    int temp = 0;
+    int count = 0;
+    int i = 1;
+    int i_reversed = degree(10, len);
+    int c = 0;
+    while(count < len)
+    {
+        c = (number / i) % 10;
+        temp += c * i_reversed;
+        i *= 10;
+        count += 1;
+        i_reversed /= 10;
+    }
+    return temp / 10;
+}
+
+int left()
+{
+    int n = degree(10, (length(number) / 2) + 1);
+    int left = number / n;
+    return left;
+}
+
+int l = left();
+
+int right()
+{
+    int right = reverse(number % degree(10, (length(number) / 2)));
+    if(right % 10 == 0){right /= 10;}
+    return right;
+}
+
+int r = right();
+
+void main(int l, int r)
+{
+    if(l == r){Console.WriteLine($"Число {number} палиндром");}
+    else{Console.WriteLine($"Число {number} не палиндром");}
+}
+
+main(l, r);
