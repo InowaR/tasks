c91f7e073d8461a9ea32381cf3cd5a5e8417f857
Georgiy <georgii_sidenko@mail.ru>
2023-02-07 00:36:23 +0300

    add 2 tasks

---
+++ lesson4/Program.cs
@@ -0,0 +1,38 @@
+﻿// // Task 25
+int degree(int num, int degree)
+{
+    int res = 1;
+    int count = 0;
+    while(count < degree)
+    {
+        res *= num;
+        count++;
+    }
+    return res;
+}
+
+
+// Task 27
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int length(int number)
+{
+    int len = 1;
+    int temp = number;
+    while (len < temp)
+    {
+        temp = temp / 10;
+        len++;
+    }
+    return len;
+}
+int len = length(number);
+int sum_numbers = 0;
+int sum = 0;
+for(int i = 1; i < len; i++)
+{
+    sum = number % degree(10, i);
+    sum /= degree(10, i) / 10;
+    sum_numbers += sum; 
+}
+Console.WriteLine(sum_numbers);
