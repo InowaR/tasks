13bf6007f237af720a64ff580a1961e4a57f0b0b
Georgiy <georgii_sidenko@mail.ru>
2023-02-11 22:06:17 +0300

    add task 38

--- lesson5/Program.cs
+++ lesson5/Program.cs
@@ -26,36 +26,72 @@
 // else if(((10+len) - (len/10) * 10) % 11 == 3){Console.WriteLine($"В массиве {len} четных числа");}
 // else {Console.WriteLine($"В массиве {len} четных чисел");}
 
-// Task 36
+// // Task 36
 
-int[] random(int[] array)
+// int[] random(int[] array)
+// {
+//   for (int i = 0; i < array.Length; i++)
+//   {
+//     array[i] = new Random().Next(10, 50);
+//   }
+//   return array;
+// }
+
+// int[] array = new int[6];
+// int[] arr = random(array);
+// foreach (int c in arr)
+// {
+//     Console.Write($"{c}, ");
+// }
+
+// List<int> odd_position = new List<int>();
+// int count = 0;
+// while(count < arr.Length)
+// {
+//     if (count % 2 == 1){odd_position.Add(arr[count]);}
+//     count++;
+// }
+// Console.WriteLine();
+// int[] odd = odd_position.ToArray();
+// int sum = 0;
+// foreach (int j in odd)
+// {
+//     sum += j;
+// }
+// Console.WriteLine($"Сумма элементов нечетных позиций в массиве = {sum}");
+
+// Task 38
+double GetPseudoDoubleWithinRange(double lowerBound, double upperBound)
 {
-  for (int i = 0; i < array.Length; i++)
-  {
-    array[i] = new Random().Next(10, 50);
-  }
-  return array;
+    var random = new Random();
+    var rDouble = random.NextDouble();
+    var rRangeDouble = rDouble * (upperBound - lowerBound) + lowerBound;
+    return rRangeDouble;
 }
 
-int[] array = new int[6];
-int[] arr = random(array);
-foreach (int c in arr)
+double[] array = new double[6];
+
+for (int i = 0; i < array.Length; i++)
 {
-    Console.Write($"{c}, ");
+array[i] = GetPseudoDoubleWithinRange(1,10);
 }
 
-List<int> odd_position = new List<int>();
-int count = 0;
-while(count < arr.Length)
+void diff_max_min()
 {
-    if (count % 2 == 1){odd_position.Add(arr[count]);}
-    count++;
+    List<double> odd_position = new List<double>();
+    double max = 0;
+    foreach (double c in array)
+    {   
+        if(c > max){max = c;}
+    }
+
+    double min = max;
+    foreach (double c in array)
+    {   
+        Console.Write($"{Math.Round(c,2)}, ");
+        if(c < min){min = c;}
+    }
+    Console.WriteLine();
+    Console.Write($"{Math.Round(max - min, 2)}");
 }
-Console.WriteLine();
-int[] odd = odd_position.ToArray();
-int sum = 0;
-foreach (int j in odd)
-{
-    sum += j;
-}
-Console.WriteLine($"Сумма элементов нечетных позиций в массиве = {sum}");
+diff_max_min();
