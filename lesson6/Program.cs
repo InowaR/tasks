8c887b75cc8162181de4007437d7058b334e6164
Georgiy <georgii_sidenko@mail.ru>
2023-02-14 21:00:50 +0300

    add task 43

--- lesson6/Program.cs
+++ lesson6/Program.cs
@@ -1,11 +1,22 @@
-﻿// Task 41
-int count = 0;
-while(true)
-{
-    Console.WriteLine("Введите число:");
-    int number = Convert.ToInt32(Console.ReadLine());
-    if(number > 0){count++;}
-    Console.WriteLine($"Введено {count} чисел больше 0");
-}
+﻿// // Task 41
+// int count = 0;
+// while(true)
+// {
+//     Console.WriteLine("Введите число:");
+//     int number = Convert.ToInt32(Console.ReadLine());
+//     if(number > 0){count++;}
+//     Console.WriteLine($"Введено {count} чисел больше 0");
+// }
 
 // Task 43
+Console.WriteLine("Введите k1:");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b1:");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2:");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2:");
+double b2 = Convert.ToDouble(Console.ReadLine());
+double x = (b1 - b2) / (k2 - k1);
+double y = (k1 * x) + b1;
+Console.WriteLine($"Точка пересечения 2 прямых x = {x}, y = {y}.");
