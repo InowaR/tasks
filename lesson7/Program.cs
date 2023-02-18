9efaf3959538a72f61bfb8bb78edc6e623c596b1
Georgiy <georgii_sidenko@mail.ru>
2023-02-18 23:06:44 +0300

    Add transposition

--- lesson7/Program.cs
+++ lesson7/Program.cs
@@ -44,10 +44,44 @@
 // }
 // CheckIndex(M);
 
-// Task 52
+// // Task 52
+// int[,] Matrix()
+// {
+//     int[,] Matrix = new int[5,4];
+//     for(int i = 0; i < Matrix.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < Matrix.GetLength(1); j++)
+//         {
+//             Matrix[i, j] = new Random().Next(100);
+//             Console.Write($"{Matrix[i,j]}\t");
+//         }
+//         Console.WriteLine();
+//     }
+//     return Matrix;
+// }
+// int[,] M = Matrix();
+// Console.WriteLine("------------------------------------");
+// int Column = 1;
+// int Sum = 0;
+// for(int i = 0; i < M.GetLength(0); i++)
+// {
+//     for(int j = Column; j < Column + 1; j++)
+//     {
+//         Console.Write($"{M[i,j]}\t");
+//         Sum += M[i,j];
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine($"Сумма элементов столбца {Column} равна {Sum}");
+
+// Extra task transposition
+
+int row = 5;
+int column = 4;
+
 int[,] Matrix()
 {
-    int[,] Matrix = new int[5,4];
+    int[,] Matrix = new int[row, column];
     for(int i = 0; i < Matrix.GetLength(0); i++)
     {
         for(int j = 0; j < Matrix.GetLength(1); j++)
@@ -60,16 +94,29 @@ int[,] Matrix()
     return Matrix;
 }
 int[,] M = Matrix();
-Console.WriteLine("---------------------------------");
-int Column = 1;
-int Sum = 0;
-for(int i = 0; i < M.GetLength(0); i++)
+Console.WriteLine("------------------------------------");
+
+int[,] T = new int[column, row];
+void GetColumn(int[,] M)
 {
-    for(int j = Column; j < Column + 1; j++)
+    for(int Column = 0; Column < M.GetLength(1); Column++)
     {
-        Console.Write($"{M[i,j]}\t");
-        Sum += M[i,j];
+        for(int i = 0; i < M.GetLength(0); i++)
+        {
+            for(int j = Column; j < Column + 1; j++)
+            {
+                T[j,i] = M[i,j];
+            }
+        }
+    }
+}
+GetColumn(M);
+
+for(int i = 0; i < T.GetLength(0); i++)
+{
+    for(int j = 0; j < T.GetLength(1); j++)
+    {
+        Console.Write($"{T[i,j]}\t");
     }
     Console.WriteLine();
 }
-Console.WriteLine($"Сумма элементов столбца {Column} равна {Sum}");
