a131904661c9cfe08f0d9c092f83977f8d9f240a
Georgiy <georgii_sidenko@mail.ru>
2023-02-18 21:35:59 +0300

    init commit

---
+++ lesson7/Program.cs
@@ -0,0 +1,11 @@
+﻿// Task 47
+double[,] matrix = new double[3,4];
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().NextDouble() * (100 + 100) - 100;
+        Console.Write($"{matrix[i,j]}\t");
+    }
+    Console.WriteLine();
+}
