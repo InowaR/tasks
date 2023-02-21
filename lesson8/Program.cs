f07ce2f67522ae533229b30dc057a48bbf55e3d7
Georgiy <georgii_sidenko@mail.ru>
2023-02-21 23:04:56 +0300

    add task 56

--- lesson8/Program.cs
+++ lesson8/Program.cs
@@ -1,6 +1,46 @@
-﻿// Task 54
+﻿// // Task 54
+// int row = 4;
+// int column = 10;
+// int[,] Matrix = new int[row, column];
+
+// for(int i = 0; i < Matrix.GetLength(0); i++)
+// {
+//     for(int j = 0; j < Matrix.GetLength(1); j++)
+//     {
+//         Matrix[i, j] = new Random().Next(1,100);
+//         Console.Write($"{Matrix[i,j]}\t");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine("----------------------------------------------------------------------------");
+
+//  void BubbleSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         for (int j = 0; j < array.Length - 1; j++)
+//             if (array[j] < array[j + 1])
+//             {
+//                 int t = array[j + 1];
+//                 array[j + 1] = array[j];
+//                 array[j] = t;
+//             }
+// }
+
+// for(int i = 0; i < Matrix.GetLength(0); i++)
+// {
+//     int[] array = new int[column];
+//     for(int j = 0; j < Matrix.GetLength(1); j++)
+//     {
+//         array[j] = Matrix[i,j];
+//     }
+//     BubbleSort(array);
+//     foreach(int element in array){Console.Write($"{element}\t");}
+//     Console.WriteLine();
+// }
+
+// Task 56
 int row = 4;
-int column = 10;
+int column = 4;
 int[,] Matrix = new int[row, column];
 
 for(int i = 0; i < Matrix.GetLength(0); i++)
@@ -12,20 +52,9 @@ for(int i = 0; i < Matrix.GetLength(0); i++)
     }
     Console.WriteLine();
 }
-Console.WriteLine("----------------------------------------------------------------------------");
+Console.WriteLine("--------------------------------------------");
 
- void BubbleSort(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-        for (int j = 0; j < array.Length - 1; j++)
-            if (array[j] < array[j + 1])
-            {
-                int t = array[j + 1];
-                array[j + 1] = array[j];
-                array[j] = t;
-            }
-}
-
+int[] SumArray = new int[row];
 for(int i = 0; i < Matrix.GetLength(0); i++)
 {
     int[] array = new int[column];
@@ -33,7 +62,17 @@ for(int i = 0; i < Matrix.GetLength(0); i++)
     {
         array[j] = Matrix[i,j];
     }
-    BubbleSort(array);
-    foreach(int element in array){Console.Write($"{element}\t");}
-    Console.WriteLine();
+    int sum = 0;
+    foreach(int element in array){sum += element;}
+    SumArray[i] = sum;
+    Console.WriteLine($"Сумма элементов строки {i} = {sum}");
 }
+Console.WriteLine("--------------------------------------------");
+int minVal = SumArray.Min();
+int indexMin = Array.IndexOf(SumArray, minVal);
+Console.WriteLine($"Строка с минимальной суммой имеет индекс {indexMin}");
+for(int count = 0; count < Matrix.GetLength(1); count++)
+{
+    Console.Write($"{Matrix[indexMin,count]}\t");
+}
+Console.WriteLine();
