6ef5383d7bef989599c5bac64cd888765d27c017
Georgiy <georgii_sidenko@mail.ru>
2023-02-27 21:40:42 +0300

    add 2 tasks

---
+++ lesson9/Program.cs
@@ -0,0 +1,20 @@
+﻿// Task 66
+int Sum(int m, int n)
+{
+    if(m == n){return m;}
+    return m + Sum(m + 1, n);
+}
+Console.WriteLine(Sum(4,8));
+
+// Task 68
+int Ackermann(int n, int m)
+{
+    while(n != 0)
+    {
+        if(m == 0){m = 1;}
+        else{m = Ackermann(n, m - 1);}
+        n--;
+    }
+    return m + 1;
+}
+Console.WriteLine(Ackermann(3,2));
